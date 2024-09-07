c6bcce1b771ab2956d7c6191c7eb2e006e6e9ecf
Gabriela Dodson <dodsongabi@gmail.com>
2024-09-06 20:37:39 -0400

    Add output functionality to display bill, tip, and total

--- cc_2.js
+++ cc_2.js
@@ -1,3 +1,4 @@
+//Calculate the tip :)
 let bill = 150;
 
 if (bill <=300 && bill >= 50) {
@@ -5,4 +6,11 @@ if (bill <=300 && bill >= 50) {
 } else if (bill > 300 || bill < 50) {
     tip = bill * 0.20;
 }
+
+
+//Output Details
+System.out.print("Bill : $" + bill);
+System.out.print("Tip: $" + tip);
+System.out.print("Total amount: $" + (bill + tip));
+
 
