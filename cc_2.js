e49585a22113f40d0af41d7d1b160a0e6dfcad26
Gabriela Dodson <dodsongabi@gmail.com>
2024-09-07 00:32:28 -0400

    Create and test calculateTip function

--- cc_2.js
+++ cc_2.js
@@ -9,8 +9,28 @@ if (bill <=300 && bill >= 50) {
 
 
 //Output Details
-System.out.print("Bill : $" + bill);
-System.out.print("Tip: $" + tip);
-System.out.print("Total amount: $" + (bill + tip));
+console.log("Bill : $" + bill);
+console.log("Tip: $" + tip);
+console.log("Total amount: $" + (bill + tip));
+
+
+
+//Create a Function
+function calculatetip (bill) {
+   let tip;
+   bill= Number(bill)
+    if (bill <=300 && bill >= 50) {
+        tip = bill *0.15;
+    } else if (bill > 300 || bill < 50) {
+        tip = bill * 0.20;
+    }
+
+return tip;
+}
+let billamount=100;
+let calculatedtip= calculatetip(billamount)
+console.log("bill: $"+billamount);
+console.log("tip: $"+ calculatedtip);
+console.log("Total amount $" +(billamount + calculatedtip));
 
 
